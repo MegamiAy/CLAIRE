fe170963a6cd3e9845ea490e8823ea4d9034fba9
Rogerio Ryu <rogerioryuss@gmail.com>
2023-10-24 16:15:20 -0300

    terminamo e ficou bunitu

--- App.js
+++ App.js
@@ -2,20 +2,37 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 
 const Menu = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Menu.Navigator>
+      <Menu.Navigator screenOptions={screenOptions}>
         <Menu.Screen name='Login' component={Login}/>
+        <Menu.Screen name='Register' component={Register}/>
       </Menu.Navigator>
     </NavigationContainer>
   );
 }
 
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#000'
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  drawerStyle: {
+    backgroundColor: '#000',
+    width: 240,
+  },
+  drawerActiveBackgroundColor: '#fff',
+  drawerActiveTintColor: '#000',
+  drawerInactiveTintColor: '#fff',
+};
 
-// testanto um bagulho 
 
   
