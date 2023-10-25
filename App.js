d3612632a35ba1f58229a9c21837c7a4e6b0c536
João Bosco <diasnetojoaobosco@gmail.com>
2023-10-25 16:51:12 -0300

    fiz a pagina de login e exclui o footer

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
-import Login from './src/screens/Login';
-import Register from './src/screens/Register';
-import HomeSrc from './src/screens/HomeSrc';
-import LogOut from './src/screens/LogOut';
-import CadProd from './src/screens/CadProd';
-import RecPass from './src/screens/RecPass';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  NavigationContainer,
+  getFocusedRouteNameFromRoute,
+} from "@react-navigation/native";
+import "react-native-gesture-handler";
+import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
+import HomeSrc from "./src/screens/HomeSrc";
+import LogOut from "./src/screens/LogOut";
+import CadProd from "./src/screens/CadProd";
+import RecPass from "./src/screens/RecPass";
 
 const Menu = createDrawerNavigator();
 
@@ -14,12 +17,24 @@ export default function App() {
   return (
     <NavigationContainer>
       <Menu.Navigator screenOptions={screenOptions}>
-        <Menu.Screen name='Login' component={Login}/>
-        <Menu.Screen name='Register' component={Register}/>
-        <Menu.Screen name='HomeScr' component={HomeSrc}/>
-        <Menu.Screen name='LogOut' component={LogOut}/>
-        <Menu.Screen name='CadProd' component={CadProd}/>
-        <Menu.Screen name='Recuperar' component={RecPass}/>
+        <Menu.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Menu.Screen
+          name="Register"
+          component={Register}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Menu.Screen name="HomeScr" component={HomeSrc} />
+        <Menu.Screen name="LogOut" component={LogOut} />
+        <Menu.Screen name="CadProd" component={CadProd} />
+        <Menu.Screen name="Recuperar" component={RecPass} />
       </Menu.Navigator>
     </NavigationContainer>
   );
@@ -27,19 +42,17 @@ export default function App() {
 
 const screenOptions = {
   headerStyle: {
-    backgroundColor: '#000'
+    backgroundColor: "#000",
   },
-  headerTintColor: '#fff',
+  headerTintColor: "#fff",
   headerTitleStyle: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   drawerStyle: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     width: 240,
   },
-  drawerActiveBackgroundColor: '#fff',
-  drawerActiveTintColor: '#000',
-  drawerInactiveTintColor: '#fff',
+  drawerActiveBackgroundColor: "#fff",
+  drawerActiveTintColor: "#000",
+  drawerInactiveTintColor: "#fff",
 };
-
-
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,70 +1,77 @@
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
-import { View, Text, TouchableOpacity,  } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import { useEffect, useState } from "react";
 import { auth } from "../config/firebase";
 import styles from "../utils/styles";
-import Footer from "../bases/Footer";
 
-export default function Login({navigation}){
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("Usuário UID: ", user.uid);
+        navigation.navigate("HomeScr");
+      } else {
+        console.log("Usuário não logado");
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                console.log("Usuário UID: ", user.uid)
-                navigation.navigate('HomeScr')
-            } else {
-                console.log("Usuário não logado")
-            }
-        })
-    }, [])
+  function handleLogin() {
+    signInWithEmailAndPassword(auth, email, senha)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigation.navigate("Home");
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    function handleLogin() {
-        signInWithEmailAndPassword(auth, email, senha)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                navigation.navigate('Home')
-                console.log(user)
-            })
-            .catch((error) => {
-                console.log(error)
-            });
-    }
-
-    return (
-        <View>
-            <View style={styles.BodyL}>
-                <TextInput
-                placeholder="Email..."
-                value={email}
-                onChangeText={setEmail}
-                style={styles.InputL}
-                />  
-                <TextInput
-                placeholder="Senha..."
-                value={senha}
-                onChangeText={setSenha}
-                style={styles.InputL}
-                secureTextEntry={true}
-                />
-                <TouchableOpacity onPress={() => navigation.navigate("Register")} style={styles.Touch}>
-                    <Text>Cadastre-se</Text>
-                </TouchableOpacity>
-                 <TouchableOpacity onPress={() => navigation.navigate("Recuperar")} style={styles.Touch}>
-                    <Text>Esqueceu a senha?</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate("CadProd")} style={styles.Touch}>
-                    <Text>TESTE</Text>
-                </TouchableOpacity>
-                <Button
-                onPress={handleLogin}
-                mode="contained"
-                style={styles.ButtonL}
-                >Logar</Button>
-            </View>    
-        </View>
-    );
+  return (
+    <View style={styles.FullBodyL}>
+        <Image source={require("../img/logo.png")} style={{...styles.imagemLR}} />
+        <Text style={styles.titleLR}>Login</Text>
+      <View style={styles.BodyL}>
+        <TextInput
+          placeholder="Email..."
+          value={email}
+          onChangeText={setEmail}
+          style={styles.InputL}
+        />
+        <TextInput
+          placeholder="Senha..."
+          value={senha}
+          onChangeText={setSenha}
+          style={styles.InputL}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Recuperar")}
+          style={styles.Touch}
+        >
+          <Text>Esqueceu a senha?</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity
+          onPress={() => navigation.navigate("CadProd")}
+          style={styles.Touch}
+        >
+          <Text>TESTE</Text>
+        </TouchableOpacity> */}
+        <Button onPress={handleLogin} mode="contained" style={styles.ButtonL}>
+          Logar
+        </Button>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Register")}
+          style={styles.Touch}
+        >
+          <Text>Cadastre-se</Text>
+        </TouchableOpacity>     
+      </View>
+    </View>
+  );
 }
